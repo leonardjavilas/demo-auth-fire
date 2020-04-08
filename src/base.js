75bd3ad1b59b58a9c0559d82120fd00ec43f3c81
Leonard J Avila S <informatic.leonardavila@gmail.com>
2020-04-08 09:09:12 -0400

    fixes

--- src/base.js
+++ src/base.js
@@ -2,12 +2,14 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID
+  apiKey: "AIzaSyBlVw-tevfxNeHbDxFd841VGH1uL1Ozvbk",
+  authDomain: "leonardjavilas.firebaseapp.com",
+  databaseURL: "https://leonardjavilas.firebaseio.com",
+  projectId: "leonardjavilas",
+  storageBucket: "leonardjavilas.appspot.com",
+  messagingSenderId: "9071395882",
+  appId: "1:9071395882:web:73d765a2fadb1c2446af53",
+  measurementId: "G-7BWVNV3SRY"
 });
 
 export default app;
